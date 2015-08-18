1fbd68a4461fa97d8d1cb8956ab3ba770cbdc8b4
ivan <ivanopt1102@gmail.com>
2015-08-18 14:48:32 +0800

    replace_temp_with_query part

---
+++ replace_temp_with_query/ReplaceTempWithQuery.java
@@ -0,0 +1,23 @@
+public class ReplaceTempWithQuery {
+
+    //before refactor
+    double quality = 1;
+    double itemPrice = 2;
+    double basePrice = quality * itemPrice;
+    //before refactor end
+
+    if (basePrice > 1) {
+        return basePrice * 0.95;
+    } else {
+        return basePrice * 0.98;    
+    }
+
+    //after refactor
+    double refactorBasePriceCount() {
+        double rQuality = 1;
+        double mItemPrice = 2;
+        return rQuality * mItemPrice; 
+    }
+    //after refactor end
+
+}
